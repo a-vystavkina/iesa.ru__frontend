c04863354c07e60ba11e8fc374c060b2ff8a7b75
a-vystavkina <al.vystavkina@gmail.com>
2022-09-23 12:11:43 +0300

    Правки

--- src/js/main.js
+++ src/js/main.js
@@ -128,3 +128,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 })
+
+const loadmore = document.querySelector('#loadmore');
+    let currentItems = 3;
+    loadmore.addEventListener('click', (e) => {
+        const elementList = [...document.querySelectorAll('.portfolio .portfolio__item')];
+        for (let i = currentItems; i < currentItems + 3; i++) {
+            if (elementList[i]) {
+                elementList[i].style.display = 'block';
+            }
+        }
+        currentItems += 3;
+        if (currentItems >= elementList.length) {
+            event.target.style.display = 'none';
+        }
+    })
